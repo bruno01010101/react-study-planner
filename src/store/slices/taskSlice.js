1a38b947c40333c4a2f5673d13987f507187dd38
Pedro Mello <contato@cicatriz.dev>
2025-08-24 16:27:28 -0300

    aula 2, video 2.4

--- src/store/slices/taskSlice.js
+++ src/store/slices/taskSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	tasks: [],
+	tasks: [
+		{
+			id: 1,
+			title: 'Task 1',
+			description: 'Description 1',
+			completed: false,
+		},
+	],
 };
 
 const taskSlice = createSlice({
@@ -20,5 +27,23 @@ const taskSlice = createSlice({
 			const task = state.tasks.find((task) => task.id === action.payload);
 			if (task) task.completed = !task.completed;
 		},
+		editTask: (state, action) => {
+			const { taskId, updatedTask } = action.payload;
+			const taskIndex = state.tasks.findIndex((task) => task.id === taskId);
+			if (taskIndex !== -1) {
+				state.tasks[taskIndex] = { ...state.tasks[taskIndex], ...updatedTask };
+			}
+		},
+		deleteTask: (state, action) => {
+			state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+		},
 	},
 });
+
+export const { addTask, toggleTaskComplete, editTask, deleteTask } = taskSlice.actions;
+
+export const selectTasks = (state) => state.tasks.tasks;
+export const selectPendingTasks = (state) => state.tasks.tasks.filter((task) => !task.completed);
+export const selectCompletedTasks = (state) => state.tasks.tasks.filter((task) => task.completed);
+
+export default taskSlice.reducer;
