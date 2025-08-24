de07160651fba972d6115f24fd8f8adc666fc8b4
Pedro Mello <contato@cicatriz.dev>
2025-08-24 17:04:01 -0300

    aula 3, video 3.3

--- src/pages/StudyPlannerPage.jsx
+++ src/pages/StudyPlannerPage.jsx
@@ -4,21 +4,25 @@ import { useTheme } from '../contexts/ThemeContext';
 import { EmptyState, ThemeToggle } from '../components/UI';
 import { AddTaskModal, EditTaskModal } from '../components/Modal';
 import { TaskSection } from '../components/Task';
-import { useSelector } from 'react-redux';
-import { selectCompletedTasks, selectPendingTasks, selectTasks } from '../store/slices/taskSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	addTask,
+	deleteTask,
+	editTask,
+	selectCompletedTasks,
+	selectPendingTasks,
+	selectTasks,
+	toggleTaskComplete,
+} from '../store/slices/taskSlice';
 
 function StudyPlannerPage() {
+	const dispatch = useDispatch();
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 	const [taskToEdit, setTaskToEdit] = useState(null);
-	// Redux
 	const tasks = useSelector(selectTasks);
 	const pendingTasks = useSelector(selectPendingTasks);
 	const completedTasks = useSelector(selectCompletedTasks);
-
-	// Context API
-	const { addTask, toggleTaskComplete, editTask, deleteTask, getPendingTasks, getCompletedTasks } =
-		useTasks();
 
 	const handleAddTask = () => {
 		setIsModalOpen(true);
@@ -29,11 +33,11 @@ function StudyPlannerPage() {
 	};
 
 	const handleAddNewTask = (newTask) => {
-		addTask(newTask);
+		dispatch(addTask(newTask));
 	};
 
 	const handleToggleComplete = (taskId) => {
-		toggleTaskComplete(taskId);
+		dispatch(toggleTaskComplete(taskId));
 	};
 
 	const handleEditTask = (taskId) => {
@@ -48,11 +52,11 @@ function StudyPlannerPage() {
 	};
 
 	const handleSaveEditTask = (taskId, updatedTask) => {
-		editTask(taskId, updatedTask);
+		dispatch(editTask({ taskId, updatedTask }));
 	};
 
 	const handleDeleteTask = (taskId) => {
-		deleteTask(taskId);
+		dispatch(deleteTask(taskId));
 	};
 
 	const theme = useTheme();
